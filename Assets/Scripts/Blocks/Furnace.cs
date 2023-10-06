a4008da6b65e60d078ca10a988954aff7dfe3960
Kogepan25629 <kogepan25629@gmail.com>
2023-10-06 22:30:08 +0900

    Fix front texture name

--- Assets/Scripts/Blocks/Furnace.cs
+++ Assets/Scripts/Blocks/Furnace.cs
@@ -3,6 +3,6 @@ public class Furnace : Block
     public static readonly string NAME = "furnace";
     public Furnace() : base(NAME)
     {
-        SetTextureAll("furnace_side", "furnace_front", "furnace_top", "furnace_top", "furnace_side", "furnace_side");
+        SetTextureAll("furnace_side", "furnace_front_off", "furnace_top", "furnace_top", "furnace_side", "furnace_side");
     }
 }
