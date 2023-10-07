98024715bab04bd7d158aca60806da37c7a42682
Kogepan25629 <kogepan25629@gmail.com>
2023-10-07 15:11:52 +0900

    ToolBarを追加

---
+++ Assets/Scripts/ItemBox.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    public int BlockID = 0;
+    private GameObject BlockIconObj;
+
+    void Start()
+    {
+        SetItem(BlockID);
+    }
+
+    public void SetItem(int blockID)
+    {
+        BlockID = blockID;
+
+        if (BlockIconObj != null)
+        {
+            Destroy(BlockIconObj);
+            BlockIconObj = null;
+        }
+
+        if (blockID == 0)
+        {
+            return;
+        }
+
+        BlockIconObj = BlockIcon.CreateBlockIcon(blockID);
+        BlockIconObj.transform.SetParent(transform);
+        BlockIconObj.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+    }
+}
