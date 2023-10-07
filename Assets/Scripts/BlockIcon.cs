356c5d86a6279189cdd982db4e2eb9105c7ae687
Kogepan25629 <kogepan25629@gmail.com>
2023-10-07 15:11:41 +0900

    アイコンの調整とGameObject生成用関数を追加

--- Assets/Scripts/BlockIcon.cs
+++ Assets/Scripts/BlockIcon.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BlockIcon : Graphic
 {
+    public const int DEFAULT_BLOCK_ICON_SIZE = 40;
+    public const int DEFAULT_BLOCK_ICON_PADDING = 5;
     public int BlockID;
     public int IconPaddingSize;
+
+    public static GameObject CreateBlockIcon(int blockID, int iconSize = DEFAULT_BLOCK_ICON_SIZE, int iconPadding = DEFAULT_BLOCK_ICON_PADDING)
+    {
+        GameObject blockIconObj = new GameObject();
+        var rect = blockIconObj.AddComponent<RectTransform>();
+        rect.sizeDelta = new Vector2 (iconSize, iconSize);
+        var blockIcon = blockIconObj.AddComponent<BlockIcon>();
+        blockIcon.BlockID = blockID;
+        blockIcon.IconPaddingSize = iconPadding;
+        blockIconObj.AddComponent<CanvasRenderer>();
+        Debug.Log("create");
+        return blockIconObj;
+    }
 
     public override Texture mainTexture
     {
@@ -23,31 +39,33 @@ public class BlockIcon : Graphic
 
     protected override void OnPopulateMesh(VertexHelper vh)
     {
+        Debug.Log("populate" + BlockID.ToString());
         vh.Clear();
         Vector3 position = transform.GetComponent<RectTransform>().anchoredPosition;
         var rectTransform = transform.GetComponent<RectTransform>();
         float iconSize = (rectTransform.sizeDelta.x < rectTransform.sizeDelta.y ? rectTransform.sizeDelta.x : rectTransform.sizeDelta.y) - IconPaddingSize * 2;
         float sizeA = iconSize * 0.2f;
         float sizeB = iconSize / 2 - sizeA;
+        float sizeC = (iconSize / 2) * 0.95f;
 
         Vector3[] vertices = new Vector3[]{
             // Top
-            new Vector3(position.x,                position.y + iconSize / 2, position.z),             // Top:1,4
-            new Vector3(position.x,                position.y + iconSize / 2 - sizeA * 2, position.z), // Top:2,5
-            new Vector3(position.x - iconSize / 2, position.y + sizeB, position.z),                    // Top:3
-            new Vector3(position.x + iconSize / 2, position.y + sizeB, position.z),                    // Top:6
+            new Vector3(position.x,         position.y + iconSize / 2, position.z),             // Top:1,4
+            new Vector3(position.x,         position.y + iconSize / 2 - sizeA * 2, position.z), // Top:2,5
+            new Vector3(position.x - sizeC, position.y + sizeB, position.z),                    // Top:3
+            new Vector3(position.x + sizeC, position.y + sizeB, position.z),                    // Top:6
 
             // Front
-            new Vector3(position.x - iconSize / 2, position.y + sizeB, position.z),                    // Front:1,4
-            new Vector3(position.x,                position.y - iconSize / 2, position.z),             // Front:2,5
-            new Vector3(position.x - iconSize / 2, position.y - sizeB, position.z),                    // Front:3
-            new Vector3(position.x,                position.y + iconSize / 2 - sizeA * 2, position.z), // Front:6
+            new Vector3(position.x - sizeC, position.y + sizeB, position.z),                    // Front:1,4
+            new Vector3(position.x,         position.y - iconSize / 2, position.z),             // Front:2,5
+            new Vector3(position.x - sizeC, position.y - sizeB, position.z),                    // Front:3
+            new Vector3(position.x,         position.y + iconSize / 2 - sizeA * 2, position.z), // Front:6
 
             // Right
-            new Vector3(position.x,                position.y + iconSize / 2 - sizeA * 2, position.z), // Right:1,4
-            new Vector3(position.x + iconSize / 2, position.y - sizeB, position.z),                    // Right:2,5
-            new Vector3(position.x,                position.y - iconSize / 2, position.z),             // Right:3
-            new Vector3(position.x + iconSize / 2, position.y + sizeB, position.x),                    // Right:6
+            new Vector3(position.x,         position.y + iconSize / 2 - sizeA * 2, position.z), // Right:1,4
+            new Vector3(position.x + sizeC, position.y - sizeB, position.z),                    // Right:2,5
+            new Vector3(position.x,         position.y - iconSize / 2, position.z),             // Right:3
+            new Vector3(position.x + sizeC, position.y + sizeB, position.x),                    // Right:6
         };
 
         var block = BlockManager.GetBlock(BlockID);
